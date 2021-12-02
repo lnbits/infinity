7bf56e141f5d606378a8f01dad530472deedb877
fiatjaf <fiatjaf@gmail.com>
2021-12-02 17:35:31 -0300

    fix load wallet balance.

--- services/balance.go
+++ services/balance.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"github.com/lnbits/lnbits/models"
 	"github.com/lnbits/lnbits/storage"
 	"gorm.io/gorm"
 )
@@ -9,12 +8,12 @@ import (
 func LoadWalletBalance(walletID string) (int64, error) {
 	var balance int64
 
-	result := storage.DB.Model(&models.Payment{}).
-		Select("coalesce(sum(amount), 0)").
-		Where("amount < 0 OR (amount > 0 AND NOT pending)").
-		Where("wallet_id = ?", walletID).
-		First(&balance)
-
+	result := storage.DB.Raw(`
+        SELECT coalesce(sum(amount), 0)
+        FROM payments
+        WHERE (amount < 0 OR (amount > 0 AND NOT pending))
+        AND wallet_id = ?
+	`, walletID).Scan(&balance)
 	if result.Error == gorm.ErrRecordNotFound {
 		return 0, nil
 	}
